b3b0966fed8b0931907e05502b1cff3cbdbcf035
William Woodard <wwoodar1@uncc.edu>
2016-11-09 23:17:37 -0500

    -modified RotatedTriangl_Matrix drawing

--- RotatedTriangle_Matrix.js
+++ RotatedTriangle_Matrix.js
@@ -9,12 +9,28 @@ var VSHADER_SOURCE =
 
 // Fragment shader program
 var FSHADER_SOURCE =
+  'precision mediump float;\n' +
+  'uniform vec4 u_FragColor;\n' +
   'void main() {\n' +
-  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
+  '  gl_FragColor = u_FragColor;\n' +
   '}\n';
 
 // The rotation angle
-var ANGLE = 90.0;
+var ANGLE = [
+  90.0, 45.0, 22.5, 11.25
+];
+
+// Hold the color data for each triangle
+var C = [
+  new Float32Array([1.0, 0.0, 0.0, 1.0]),
+  new Float32Array([0.0, 1.0, 0.0, 1.0]),
+  new Float32Array([0.0, 0.0, 1.0, 1.0]),
+  new Float32Array([0.5, 0.5, 0.5, 1.0])
+];
+
+// Hold the raw vertices
+var _vertices = [];
+var _n = 0; // The number of vertices
 
 function main() {
   // Retrieve <canvas> element
@@ -32,6 +48,14 @@ function main() {
     console.log('Failed to intialize shaders.');
     return;
   }
+
+  for (var i = 0; i < ANGLE.length; i++) {
+    // Push new vertices (triangle)
+    _vertices.push(
+      0, 0.5,   -0.5, -0.5,   0.5, -0.5
+    );
+    _n = _n + 3; // The number of vertices
+  }
  
   // Write the positions of vertices to a vertex shader
   var n = initVertexBuffers(gl);
@@ -40,25 +64,19 @@ function main() {
     return;
   }
 
-  // Create a rotation matrix
-  var radian = Math.PI * ANGLE / 180.0; // Convert to radians
-  var cosB = Math.cos(radian), sinB = Math.sin(radian);
-
-  // Note: WebGL is column major order
-  var xformMatrix = new Float32Array([
-     cosB, sinB, 0.0, 0.0,
-    -sinB, cosB, 0.0, 0.0,
-      0.0,  0.0, 1.0, 0.0,
-      0.0,  0.0, 0.0, 1.0
-  ]);
-
   // Pass the rotation matrix to the vertex shader
   var u_xformMatrix = gl.getUniformLocation(gl.program, 'u_xformMatrix');
   if (!u_xformMatrix) {
     console.log('Failed to get the storage location of u_xformMatrix');
     return;
   }
-  gl.uniformMatrix4fv(u_xformMatrix, false, xformMatrix);
+
+  // Pass the color vector
+  var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+  if (!u_FragColor) {
+    console.log('Failed to get the storage location of u_FragColor');
+    return;
+  }
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0, 0, 0, 1);
@@ -66,15 +84,37 @@ function main() {
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  // Draw the rectangle
-  gl.drawArrays(gl.TRIANGLES, 0, n);
+  // Hold current triangle number
+  var currentTriangle = 0;
+  for (var i = 0; i < ANGLE.length; i++) {
+    // Create a rotation matrix
+    var radian = Math.PI * ANGLE[i] / 180.0; // Convert to radians
+    var cosB = Math.cos(radian), sinB = Math.sin(radian);
+
+    // Note: WebGL is column major order
+    var xformMatrix = new Float32Array([
+       cosB, sinB, 0.0, 0.0,
+      -sinB, cosB, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0
+    ]);
+
+    gl.uniformMatrix4fv(u_xformMatrix, false, xformMatrix);
+
+    // Set color of triangle
+    gl.uniform4fv(u_FragColor, C[i]);
+
+    // Draw the rectangle
+    gl.drawArrays(gl.TRIANGLES, currentTriangle, 3);
+
+    // Go to next triangle in vertex array
+    currentTriangle += 3;
+  }
 }
 
 function initVertexBuffers(gl) {
-  var vertices = new Float32Array([
-    0, 0.5,   -0.5, -0.5,   0.5, -0.5
-  ]);
-  var n = 3; // The number of vertices
+  var vertices = new Float32Array(_vertices);
+  var n = _n; // The number of vertices
 
   // Create a buffer object
   var vertexBuffer = gl.createBuffer();
