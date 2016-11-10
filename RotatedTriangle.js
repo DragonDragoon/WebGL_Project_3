a90ce079f44faba2ef9b225f055dae4564d68400
William Woodard <wwoodar1@uncc.edu>
2016-11-09 22:57:05 -0500

    - modified RotatedTriangle drawing

--- RotatedTriangle.js
+++ RotatedTriangle.js
@@ -15,12 +15,28 @@ var VSHADER_SOURCE =
 
 // Fragment shader program
 var FSHADER_SOURCE =
+  'precision mediump float;\n' +
+  'uniform vec4 u_FragColor;\n' +
   'void main() {\n' +
-  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
+  '  gl_FragColor = u_FragColor;\n' +
   '}\n';
 
 // The rotation angle
-var ANGLE = 90.0; 
+var ANGLE = [
+  90.0, 45.0, 22.5, 11.25
+];
+
+// Hold the color data for each triangle
+var C = [
+  new Float32Array([1.0, 0.0, 0.0, 1.0]),
+  new Float32Array([0.0, 1.0, 0.0, 1.0]),
+  new Float32Array([0.0, 0.0, 1.0, 1.0]),
+  new Float32Array([0.5, 0.5, 0.5, 1.0])
+];
+
+// Hold the raw vertices
+var _vertices = [];
+var _n = 0; // The number of vertices
 
 function main() {
   // Retrieve <canvas> element
@@ -39,6 +55,14 @@ function main() {
     return;
   }
 
+  for (var i = 0; i < ANGLE.length; i++) {
+    // Push new vertices (triangle)
+    _vertices.push(
+      0, 0.5,   -0.5, -0.5,   0.5, -0.5
+    );
+    _n = _n + 3; // The number of vertices
+  }
+
   // Write the positions of vertices to a vertex shader
   var n = initVertexBuffers(gl);
   if (n < 0) {
@@ -47,18 +71,19 @@ function main() {
   }
 
   // // Pass the data required to rotate the shape to the vertex shader
-  var radian = Math.PI * ANGLE / 180.0; // Convert to radians
-  var cosB = Math.cos(radian);
-  var sinB = Math.sin(radian);
-
   var u_CosB = gl.getUniformLocation(gl.program, 'u_CosB');
   var u_SinB = gl.getUniformLocation(gl.program, 'u_SinB');
   if (!u_CosB || !u_SinB) {
     console.log('Failed to get the storage location of u_CosB or u_SinB');
     return;
   }
-  gl.uniform1f(u_CosB, cosB);
-  gl.uniform1f(u_SinB, sinB);
+
+  // Pass the color vector
+  var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+  if (!u_FragColor) {
+    console.log('Failed to get the storage location of u_FragColor');
+    return;
+  }
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0, 0, 0, 1);
@@ -66,15 +91,29 @@ function main() {
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  // Draw the rectangle
-  gl.drawArrays(gl.TRIANGLES, 0, n);
+  // Hold current triangle number
+  var currentTriangle = 0;
+  for (var i = 0; i < ANGLE.length; i++) {
+    var radian = Math.PI * ANGLE[i] / 180.0; // Convert to radians
+    var cosB = Math.cos(radian);
+    var sinB = Math.sin(radian);
+    gl.uniform1f(u_CosB, cosB);
+    gl.uniform1f(u_SinB, sinB);
+
+    // Set color of triangle
+    gl.uniform4fv(u_FragColor, C[i]);
+
+    // Draw the rectangle
+    gl.drawArrays(gl.TRIANGLES, currentTriangle, 3);
+
+    // Go to next triangle in vertex array
+    currentTriangle += 3;
+  }
 }
 
 function initVertexBuffers(gl) {
-  var vertices = new Float32Array([
-    0, 0.5,   -0.5, -0.5,   0.5, -0.5
-  ]);
-  var n = 3; // The number of vertices
+  var vertices = new Float32Array(_vertices);
+  var n = _n; // The number of vertices
 
   // Create a buffer object
   var vertexBuffer = gl.createBuffer();
